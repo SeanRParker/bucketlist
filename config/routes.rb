291810f5e848a9581800b7e854c1b40e0b2f96cf
Mara Behles Dean <maraebehles@gmail.com>
2016-06-02 14:23:09 -0600

    configure routes for registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 root 'static#index'
-  devise_for :users
+  devise_for :users, controllers: {
+        registrations: 'users/registrations'
 
   resources :lists do
     resources :items
